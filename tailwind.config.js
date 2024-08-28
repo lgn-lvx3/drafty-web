a6d765e0db3cca7ef3ff7a193a601bacf578dfe8
Logan Hendershot <logan@lvx3.com>
2024-08-28 14:26:59 -0700

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{tsx,ts}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        sm: "url('./assets/img/bg-sm.png')",
-        md: "url('./assets/img/bg-md.png')",
-      },
-    },
-  },
-  plugins: [require('daisyui')],
+	content: [
+		"./index.html",
+		"./src/**/*.{tsx,ts}",
+		"node_modules/daisyui/dist/**/*.js",
+		"node_modules/react-daisyui/dist/**/*.js",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				sm: "url('./assets/img/bg-sm.png')",
+				md: "url('./assets/img/bg-md.png')",
+			},
+		},
+	},
+	plugins: [require("daisyui")],
 };
